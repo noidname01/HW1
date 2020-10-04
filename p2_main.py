14588365805a6b5672ca9eed253af12ee8b15742
Sologa <b04901114@ntu.edu.tw>
2020-10-04 21:16:29 +0800

    Update p2_main.py

--- p2_main.py
+++ p2_main.py
@@ -2,20 +2,25 @@ import sys
 from graph_gen import *
 from p2 import p2_has_cycle
 
+
 def main():
     p2_list = list()
     if len(sys.argv) <= 1:
-        p2_list = get_p2('r07')
+        p1_list = get_p2('r07')
     else:
-        p2_list = get_p2(sys.argv[1])
+        p1_list = get_p2(sys.argv[1])
+        
+    p1_list_converted = convert_p2(p2_list)
+    for i in range(len(p2_list)):
+        graph = nx.DiGraph(p2_list[i])
+        has_cycle = True
+        try:
+            res = nx.find_cycle(graph)
+        except:
+            has_cycle = False
+        if p1_has_cycle(p2_list_converted[i]) != has_cycle:
+            print('Bug in the {}th graph'.format(i))
     
-    with open(sys.argv[1] + '_p2.txt', 'w') as f2:
-        for sets in p2_list:
-            if p2_has_cycle(sets):
-                f2.write('Yes')
-            else:
-                f2.write('No')
-            f2.write('\n')
 
 if __name__ == '__main__':
     main()
