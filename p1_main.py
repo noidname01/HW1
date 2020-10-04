31e299659b69f808c54c5339330d470f98d06542
Sologa <b04901114@ntu.edu.tw>
2020-10-04 21:14:06 +0800

    Update p1_main.py

--- p1_main.py
+++ p1_main.py
@@ -2,6 +2,7 @@ import sys
 from graph_gen import *
 from p1 import p1_has_cycle
 import networkx as nx
+import pdb
 
 
 def main():
@@ -11,13 +12,17 @@ def main():
     else:
         p1_list = get_p1(sys.argv[1])
         
-    with open(sys.argv[1] + '_p1.txt', 'w') as f1:
-        for sets in p1_list:
-            if p1_has_cycle(sets):
-                f1.write('Yes')
-            else:
-                f1.write('No')
-            f1.write('\n')
+    p1_list_converted = convert_p1(p1_list)
+    for i in range(len(p1_list)):
+        graph = nx.DiGraph(p1_list[i])
+        has_cycle = True
+        try:
+            res = nx.find_cycle(graph)
+        except:
+            has_cycle = False
+        if p1_has_cycle(p1_list_converted[i]) != has_cycle:
+            pdb.set_trace()
+    print('All clear!!')
 
 if __name__ == '__main__':
     main()
