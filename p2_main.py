8f206362301bb7c363df08b00d395e71f80392cf
noidname <tim983649@gmail.com>
2020-10-11 21:25:47 +0800

    update p2_main

--- p2_main.py
+++ p2_main.py
@@ -7,11 +7,11 @@ import networkx as nx
 def main():
     p2_list = list()
     if len(sys.argv) <= 1:
-        p1_list = get_p2('r07')
+        p2_list = get_p2('r07')
     else:
-        p1_list = get_p2(sys.argv[1])
-        
-    p1_list_converted = convert_p2(p2_list)
+        p2_list = get_p2(sys.argv[1])
+
+    p2_list_converted = convert_p2(p2_list)
     for i in range(len(p2_list)):
         graph = nx.DiGraph(p2_list[i])
         has_cycle = True
@@ -19,9 +19,9 @@ def main():
             res = nx.find_cycle(graph)
         except:
             has_cycle = False
-        if p1_has_cycle(p2_list_converted[i]) != has_cycle:
+        if p2_has_cycle(p2_list_converted[i]) != has_cycle:
             print('Bug in the {}th graph. P2.'.format(i))
-    
+
 
 if __name__ == '__main__':
     main()
